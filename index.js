141b54a1263a8e6cacf51a1302576930e8c8c6b3
adir-avraham <adir.avraham@gmail.com>
2019-08-11 13:35:07 +0300

    cosmetic

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const TASKS_DOM = {
     task_form: document.getElementById("myForm")
 }
 
-
 let arrayOfData;
-
 
 function draw(arrayOfData) {
     clearTask()
@@ -18,11 +16,9 @@ function draw(arrayOfData) {
     }
 }
 
-
 function clearTask() {
     TASKS_DOM.tasks_data.innerHTML = "";
 }
-
 
 function drawLi(task) {
     const { tasks_data } = TASKS_DOM;
@@ -31,15 +27,12 @@ function drawLi(task) {
     tasks_data.append(taskUL);
 }
 
-
 function deleteTask(id) {
     const index = findIndex(arrayOfData, id);
-    if (id === undefined) return;
     arrayOfData.splice(index, 1);
     saveToLocalStorage("tasksData", arrayOfData)
     draw(arrayOfData);
 }
-
 
 function findIndex(data, id) {
     for (let index = 0; index < data.length; index++) {
@@ -49,14 +42,13 @@ function findIndex(data, id) {
     }
 }
 
-
 function createTask(task) {
     const { task_name, task_description, task_date, task_time, task_id } = task;
     
     const divElement = document.createElement("div");
-
     divElement.className = "mt-5 card-body card-hover sticky-note m-3 ml-5 col-lg-3";
     divElement.style = "max-width: 17rem";
+    divElement.id = task_id;
     divElement.addEventListener("mouseover", function(){
     deleteButton.style.display = "inline-block";})      
     divElement.addEventListener("mouseleave", function(){
@@ -65,7 +57,7 @@ function createTask(task) {
     const deleteButton = document.createElement("Button")
     deleteButton.className = "btn btn-danger button-no-paddind far fa-trash-alt float-right";
     deleteButton.style.display = "none";  
-    deleteButton.addEventListener("click", deleteTaskHandler);
+    deleteButton.addEventListener("click", function(){deleteTask(this.parentElement.parentElement.id)});
 
     const card_task_name = document.createElement("h3");
     card_task_name.innerText = task_name;
@@ -82,41 +74,28 @@ function createTask(task) {
     const card_task_time = document.createElement("footer");
     card_task_time.innerText = task_time;
 
-    divElement.id = task_id;
-
-    divElement.append( card_task_name , card_task_description, card_task_date, card_task_time);
+    divElement.append( card_task_name, card_task_description, card_task_date, card_task_time);
 
     return divElement;
 }
 
-
-function deleteTaskHandler() {
-    deleteTask(this.parentElement.parentElement.id);
-}
-
-
 document.querySelector("#addButton").addEventListener("click", saveTask);
-
 
 function saveTask() {
     const { task_name, task_description, task_date, task_time } = TASKS_DOM;
-
     if (!task_name.value || !task_description.value || !task_date.value || !task_time.value) {
         alert("Please fill out the form")
        return;
     }
-
-    arrayOfData.push(new Task(task_name.value, task_description.value, task_date.value, task_time.value))
-    saveToLocalStorage("tasksData", arrayOfData)
-    draw(arrayOfData)
+    arrayOfData.push(new Task(task_name.value, task_description.value, task_date.value, task_time.value));
+    saveToLocalStorage("tasksData", arrayOfData);
+    draw(arrayOfData);
     TASKS_DOM.task_form.reset();
 }
-
 
 function saveToLocalStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
-
 
 function Task(_name, _description, _date, _time) {
     this.task_name = _name;
@@ -124,9 +103,8 @@ function Task(_name, _description, _date, _time) {
     this.task_date = _date;
     this.task_time = _time;
     const rand = Math.round(Math.random() * 99999);
-    this.task_id = `note_${rand}`
+    this.task_id = `note_${rand}`;
 }
-
 
 function init() {
     arrayOfData = JSON.parse(localStorage.getItem("tasksData")) || []
